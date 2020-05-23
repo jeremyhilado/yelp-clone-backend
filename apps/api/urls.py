c65b5e401a33c6c862f4d3b7788215fd5d4907a0
Jeremy Hilado <jhilado89@gmail.com>
2020-05-23 16:20:41 -0700

    added patch view

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from apps.api.views import (
-    BusinessViewSet, ReviewViewSet, PublicBusinesses, PublicBusinessDetail,
-    PublicReviews, PublicReviewDetail, SearchDatabase, ImageViewSet
+    BusinessViewSet, ReviewViewSet, SearchDatabase, ImageViewSet,
+    BusinessEdit
 )
 router = DefaultRouter()
 router.register('businesses', BusinessViewSet, basename='businesses')
@@ -10,10 +10,7 @@ router.register('reviews', ReviewViewSet, basename='reviews')
 router.register('images', ImageViewSet, basename='images')
 
 custom_urlpatterns = [
-    url(r'public-businesses/$', PublicBusinesses.as_view(), name='public_businesses'),
-    url(r'public_businesses/(?P<pk>\d+)/$', PublicBusinessDetail.as_view(), name='public_business_detail'),
-    url(r'public-reviews/$', PublicReviews.as_view(), name='public_reviews'),
-    url(r'public-reviews/(?P<pk>\d+)/$', PublicReviewDetail.as_view(), name='public_review_detail'),
+    url(r'items/(?P<pk>\d+)/$', BusinessEdit.as_view(), name='business-edit'),
     url(r'search/$', SearchDatabase.as_view(), name='search')
 ]
 
